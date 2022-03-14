7176a448731ffeabc48f9e1aa4041217a43f0591
Anar Abdullayev <anabdu@ttu.ee>
2022-03-14 09:21:52 +0200

    i2c

---
+++ dsp_black_box/I2C_V1/i2c_ex3_external_loopback/CPU1_RAM/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../2837xD_RAM_lnk_cpu1.cmd 
+
+LIB_SRCS += \
+C:/ti/C2000Ware_3_04_00_00_Software/driverlib/f2837xd/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../i2c_ex3_external_loopback.c 
+
+C_DEPS += \
+./i2c_ex3_external_loopback.d 
+
+OBJS += \
+./i2c_ex3_external_loopback.obj 
+
+OBJS__QUOTED += \
+"i2c_ex3_external_loopback.obj" 
+
+C_DEPS__QUOTED += \
+"i2c_ex3_external_loopback.d" 
+
+C_SRCS__QUOTED += \
+"../i2c_ex3_external_loopback.c" 
+
+
