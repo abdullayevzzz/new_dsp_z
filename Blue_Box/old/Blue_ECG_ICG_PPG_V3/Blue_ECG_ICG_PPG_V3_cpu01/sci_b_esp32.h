d43b215fc1511ed53d13c5fc195aa402a328ce04
abdullayevzzz <abdullayev.1995@gmail.com>
2024-04-02 14:14:11 +0300

    v4_added

---
+++ Blue_Box/old/Blue_ECG_ICG_PPG_V3/Blue_ECG_ICG_PPG_V3_cpu01/sci_b_esp32.h
@@ -0,0 +1,13 @@
+/*
+ * spi_esp32.h
+ *
+ *  Created on: Feb 20, 2024
+ *      Author: admin
+ */
+
+#ifndef SCI_B_ESP32_H_
+#define SCI_B_ESP32_H_
+
+void init_sci_b(void);
+
+#endif /* SCI_B_ESP32_H_ */
